104151d7bf838ba14153f9b08de6ed1c7404dbf7
bhanu <kasthuribhanupraksh07@gmail.com>
2026-08-19 04:25:22 +0530

    Fix template directory for deployment

--- vendormangement/settings.py
+++ vendormangement/settings.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'vendormangement.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR/"templates"],
+        'DIRS': [BASE_DIR/"vendor"/"templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
